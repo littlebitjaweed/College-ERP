965dbc5d4fe3fb608d8f318723c39b91c6bb6f4c
Taher9845 <mohammadtaher873@gmail.com>
2024-12-09 00:37:43 +0530

    Updated UI design

--- CollegeERP/urls.py
+++ CollegeERP/urls.py
@@ -5,10 +5,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
-    path('info/', include('info.urls')),
+    path('info/', include('info.urls')), 
     path('api/', include('apis.urls')),
     path('accounts/login/',
          auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
     path('accounts/logout/',
-         auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout'),
+         auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout')
 ]
